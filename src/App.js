0ea575d2bd37b329711707ef8d816a60c4572863
vivek <vivek.koyani@gmail.com>
2020-09-12 12:00:26 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      //BEM naming convention
+
+      <h1>Hello</h1>
+
+
+      {/* header */}
+      {/* sidebar */}
+      {/* React-Router -> Chat screen */}
+    </div>
+  )
+}
+
+export default App;
