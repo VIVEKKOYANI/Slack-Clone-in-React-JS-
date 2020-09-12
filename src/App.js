034c281d9be88ce4a2ca3ef417cadebb10c1f122
vivek <vivek.koyani@gmail.com>
2020-09-12 12:01:50 +0530

    formate code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <div>
-      //BEM naming convention
+      {/*BEM naming convention*/}
 
       <h1>Hello</h1>
-
 
       {/* header */}
       {/* sidebar */}
       {/* React-Router -> Chat screen */}
     </div>
-  )
+  );
 }
 
 export default App;
